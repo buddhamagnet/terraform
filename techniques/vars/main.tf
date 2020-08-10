a3f23948f0605b1348fc40e1a443efbd13ee2e8b
Dave Goodchild <david.goodchild@directlinegroup.co.uk>
2020-08-10 12:17:39 +0100

    use var files

---
+++ techniques/vars/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+# terraform plan -var-file=(dev|stage|prod).vars
+variable "instances" {
+    type = number
+}
+
+resource "aws_instance" "demo_instance" {
+    ami = "ami-0a13d44dccf1f5cf6"
+    instance_type = "t2.micro"
+    count = var.instances
+}
